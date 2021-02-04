6c1b4e862d83f2bc0494d3cc1e9595c17cb0d2b1
rikuto1020 <67956343+rikuto1020@users.noreply.github.com>
2021-02-04 20:18:30 +0900

    月ごとの表示ローティン編集しました

---
+++ app/javascript/body.js
@@ -0,0 +1,26 @@
+$(document).on('turbolinks:load', ()=> {
+
+  // var id = $('month_select').attr('id');
+  $(function() {
+  
+
+//     const html = ` <a href="/index/index">
+//     <i class="icon-home"></i>Home
+// </a>`;
+
+//     return html;
+
+    //セレクトボックスが切り替わったら発動
+    $('[name=budget_d]').change(function() {
+      var val = $('[name=budget_d]').val();
+      //選択したvalue値を変数に格納
+      // var val = $(this).id();
+      console.log(val);
+      if ( val == 1) { 
+      //選択したvalue値をp要素に出力
+      $('.tempereture').append('<a href="/index/index" <i class="icon-home"></i>Home</a>');}
+
+
+    });
+    });
+  });
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,22 @@ Rails.application.routes.draw do
   root "temperatures#index"
   resources :users, only: [:edit, :update]
 
+  resources :temperaturas, only: [:index, :new, :create] do
+    member do
+      get 'one', to: 'temperaturas#one'
+      get 'two', to: 'temperaturas#two'
+      get 'three', to: 'temperaturas#three'
+      get 'four', to: 'temperaturas#four'
+      get 'five', to: 'temperaturas#five'
+      get 'six', to: 'temperaturas#six'
+      get 'seven', to: 'temperaturas#seven'
+      get 'eight', to: 'temperaturas#eight'
+      get 'nine', to: 'temperaturas#nine'
+      get 'ten', to: 'temperaturas#ten'
+      get 'eleven', to: 'temperaturas#eleven'
+      get 'twelve', to: 'temperaturas#twelve'
+    end
+  end
+
+
 end
