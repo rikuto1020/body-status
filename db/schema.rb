6ab885560e521b24a4045b3a35940d57ff78d9dc
rikuto1020 <67956343+rikuto1020@users.noreply.github.com>
2021-02-06 19:58:31 +0900

    更新

--- db/schema.rb
+++ db/schema.rb
@@ -10,35 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< Updated upstream
-ActiveRecord::Schema.define(version: 2021_01_26_105531) do
-
-  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.string "size"
-    t.string "status"
-    t.integer "price"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-=======
-ActiveRecord::Schema.define(version: 2021_02_06_105148) do
-
-  create_table "group_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "group_id"
-    t.bigint "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["group_id"], name: "index_group_users_on_group_id"
-    t.index ["user_id"], name: "index_group_users_on_user_id"
-  end
-
-  create_table "groups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["name"], name: "index_groups_on_name", unique: true
->>>>>>> Stashed changes
-  end
+ActiveRecord::Schema.define(version: 2021_01_26_041558) do
 
   create_table "temperaturas", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.float "temperature", null: false
@@ -63,6 +35,4 @@ ActiveRecord::Schema.define(version: 2021_02_06_105148) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "group_users", "groups"
-  add_foreign_key "group_users", "users"
 end
