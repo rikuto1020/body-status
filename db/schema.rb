c7b24889580791c01143a72fd4b56cef267ad08f
rikuto1020 <67956343+rikuto1020@users.noreply.github.com>
2021-02-10 21:57:17 +0900

    変更

--- db/schema.rb
+++ db/schema.rb
@@ -48,8 +48,6 @@ ActiveRecord::Schema.define(version: 2021_02_06_110212) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "group_users", "groups"
