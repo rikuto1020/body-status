2f2dcb8dd5ed45431a2234be7478ffa6ac891791
rikuto1020 <67956343+rikuto1020@users.noreply.github.com>
2021-02-14 21:54:04 +0900

    変更

--- app/controllers/temperaturas_controller.rb
+++ app/controllers/temperaturas_controller.rb
@@ -17,25 +17,33 @@ class TemperaturasController < ApplicationController
 
 
   def one
-    @data_1 = Temperatura.where(month_id: 1)
-    @data1_select = @data_1.select(:temperature)
-    
+    @data_1 = Temperatura.where(month_id: 1).where(user_id: current_user)
+    @temperature = @data_1.select(:temperature)
+    @result_data = @data_1.select(:result_date)
   end
   
   def two
-    @data_2 = Temperatura.where(month_id: 2)
+    @data_2 = Temperatura.where(month_id: 2).where(user_id: current_user)
+    @temperature = @data_2.select(:temperature)
+    @result_data = @data_2.select(:result_date)
   end
 
   def three 
-    @data_3 = Temperatura.where(month_id: 3)
+    @data_3 = Temperatura.where(month_id: 3).where(user_id: current_user)
+    @temperature = @data_3.select(:temperature)
+    @result_data = @data_3.select(:result_date)
   end
 
   def four
-    @data_4 = Temperatura.where(month_id: 4)
+    @data_4 = Temperatura.where(month_id: 4).where(user_id: current_user)
+    @temperature = @data_4.select(:temperature)
+    @result_data = @data_4.select(:result_date)
   end
 
   def five
-    @data_5 = Temperatura.where(month_id: 5)
+    @data_5 = Temperatura.where(month_id: 5).where(user_id: current_user)
+    @temperature = @data_5.select(:temperature)
+    @result_data = @data_5.select(:result_date)
   end
   
   def six
@@ -45,27 +53,39 @@ class TemperaturasController < ApplicationController
   end
 
   def seven
-    @data_7 = Temperatura.where(month_id: 7)
+    @data_7 = Temperatura.where(month_id: 7).where(user_id: current_user)
+    @temperature = @data_7.select(:temperature)
+    @result_data = @data_7.select(:result_date)
   end
 
   def eight
-    @data_8 = Temperatura.where(month_id: 8)
+    @data_8 = Temperatura.where(month_id: 8).where(user_id: current_user)
+    @temperature = @data_8.select(:temperature)
+    @result_data = @data_8.select(:result_date)
   end
 
   def nine
-    @data_9 = Temperatura.where(month_id: 9)
+    @data_9= Temperatura.where(month_id: 9).where(user_id: current_user)
+    @temperature = @data_9.select(:temperature)
+    @result_data = @data_9.select(:result_date)
   end
 
   def ten
-    @data_10 = Temperatura.where(month_id: 10)
+    @data_10 = Temperatura.where(month_id: 10).where(user_id: current_user)
+    @temperature = @data_10.select(:temperature)
+    @result_data = @data_10.select(:result_date)
   end
 
   def eleven
-    @data_11 = Temperatura.where(month_id: 11)
+    @data_11 = Temperatura.where(month_id: 11).where(user_id: current_user)
+    @temperature = @data_11.select(:temperature)
+    @result_data = @data_11.select(:result_date)
   end
 
   def twelve
-    @data_12 = Temperatura.where(month_id: 12)
+    @data_12 = Temperatura.where(month_id: 12).where(user_id: current_user)
+    @temperature = @data_12.select(:temperature)
+    @result_data = @data_12.select(:result_date)
   end
 
 
--- app/javascript/body.js
+++ app/javascript/body.js
@@ -15,55 +15,55 @@ $(document).on('turbolinks:load', ()=> {
       var val = $('[name=budget_d]').val();
       //選択したvalue値を変数に格納
       // var val = $(this).id();
-      $('.icon-home').remove()
+      $('.java-text').remove()
       console.log(val);
       if ( val == 1) { 
       //選択したvalue値をp要素に出力
-      $('.tempereture').append('<a href="/groups/:group_id/temperaturas/:id/one" <i class="text"></i>1月の体温記録を見る</a>');}
+      $('.tempereture').append('<a href="/groups/:group_id/temperaturas/:id/one" <i class="java-text"></i>1月の体温記録を見る</a>');}
 
       if ( val == 2) { 
         //選択したvalue値をp要素に出力
-        $('.tempereture').append('<a href="/groups/:group_id/temperaturas/:id/two" <i class="text"></i>２月の体温記録を見る</a>');}
+        $('.tempereture').append('<a href="/groups/:group_id/temperaturas/:id/two" <i class="java-text"></i>２月の体温記録を見る</a>');}
 
       if ( val == 3) { 
         //選択したvalue値をp要素に出力
-        $('.tempereture').append('<a href="/groups/:group_id/temperaturas/:id/three" <i class="text"></i>３月の体温記録を見る</a>');}
+        $('.tempereture').append('<a href="/groups/:group_id/temperaturas/:id/three" <i class="java-text"></i>３月の体温記録を見る</a>');}
 
       if ( val == 4) { 
         //選択したvalue値をp要素に出力
-        $('.tempereture').append('<a href="/groups/:group_id/temperaturas/:id/four" <i class="text"></i>４月の体温記録を見る</a>');}
+        $('.tempereture').append('<a href="/groups/:group_id/temperaturas/:id/four" <i class="java-text"></i>４月の体温記録を見る</a>');}
 
       if ( val == 5) { 
         //選択したvalue値をp要素に出力
-        $('.tempereture').append('<a href=/groups/:group_id/temperaturas/:id/five" <i class="text"></i>５月の体温記録を見る</a>');}
+        $('.tempereture').append('<a href=/groups/:group_id/temperaturas/:id/five" <i class="java-text"></i>５月の体温記録を見る</a>');}
 
       if ( val == 6) { 
         //選択したvalue値をp要素に出力
-        $('.tempereture').append('<a href="/groups/:group_id/temperaturas/:id/six" <i class="text"></i>６月の体温記録を見る</a>');}
+        $('.tempereture').append('<a href="/groups/:group_id/temperaturas/:id/six" <i class="java-text"></i>６月の体温記録を見る</a>');}
 
         if ( val == 7) { 
           //選択したvalue値をp要素に出力
-          $('.tempereture').append('<a href="/groups/:group_id/temperaturas/:id/seven" <i class="text"></i>７月の体温記録を見る</a>');}
+          $('.tempereture').append('<a href="/groups/:group_id/temperaturas/:id/seven" <i class="java-text"></i>７月の体温記録を見る</a>');}
 
         if ( val == 8) { 
           //選択したvalue値をp要素に出力
-          $('.tempereture').append('<a href="/groups/:group_id/temperaturas/:id/eight" <i class="text"></i>８月の体温記録を見る</a>');}
+          $('.tempereture').append('<a href="/groups/:group_id/temperaturas/:id/eight" <i class="java-text"></i>８月の体温記録を見る</a>');}
 
         if ( val == 9) { 
           //選択したvalue値をp要素に出力
-          $('.tempereture').append('<a href="/groups/:group_id/temperaturas/:id/nine" <i class="text"></i>９月の体温記録を見る</a>');}
+          $('.tempereture').append('<a href="/groups/:group_id/temperaturas/:id/nine" <i class="java-text"></i>９月の体温記録を見る</a>');}
 
         if ( val == 10) { 
           //選択したvalue値をp要素に出力
-          $('.tempereture').append('<a href="/groups/:group_id/temperaturas/:id/ten" <i class="text"></i>１０月の体温記録を見る</a>');}
+          $('.tempereture').append('<a href="/groups/:group_id/temperaturas/:id/ten" <i class="java-text"></i>１０月の体温記録を見る</a>');}
 
         if ( val == 11) { 
           //選択したvalue値をp要素に出力
-          $('.tempereture').append('<a href="/groups/:group_id/temperaturas/:id/eleven" <i class="text"></i>１１月の体温記録を見る</a>');}
+          $('.tempereture').append('<a href="/groups/:group_id/temperaturas/:id/eleven" <i class="java-text"></i>１１月の体温記録を見る</a>');}
 
         if ( val == 12) { 
           //選択したvalue値をp要素に出力
-          $('.tempereture').append('<a href=" /groups/:group_id/temperaturas/:id/twelve" <i class="text"></i>１２月の体温記録を見る</a>');}
+          $('.tempereture').append('<a href=" /groups/:group_id/temperaturas/:id/twelve" <i class="java-text"></i>１２月の体温記録を見る</a>');}
 
     });
     });
